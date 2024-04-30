808aab9a85b27ac2c6c4606200729e72bd81c55a
TPGPL <TPGPL@users.noreply.github.com>
2024-04-30 21:56:34 +0200

    Add basic site layout

---
+++ src/components/layout/UserPanel.jsx
@@ -0,0 +1,56 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {logoutAction} from "../../actionCreators/auth.jsx";
+import {ActionIcon, Group, Menu, rem, Stack, Text, useMantineColorScheme} from "@mantine/core";
+import {IconLogin, IconMoon, IconSun, IconUserFilled} from "@tabler/icons-react";
+
+export default function UserPanel() {
+    let dispatch = useDispatch();
+    let navigate = useNavigate();
+    let userData = useSelector(s => s.account.userData);
+    let {colorScheme, setColorScheme} = useMantineColorScheme();
+
+    function toggleTheme() {
+        setColorScheme(colorScheme === 'light' ? 'dark' : 'light')
+    }
+
+    function logout() {
+        fetch('http://localhost:8080/api/logout', {method: "POST"})
+            .finally(() => {
+                dispatch(logoutAction());
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <Group>
+            <Stack gap={0} justify="flex-start">
+                <Text fw={700} ta="right">{userData?.surname}</Text>
+                <Text ta="right">{userData?.name}</Text>
+            </Stack>
+            <Menu width={200}>
+                <Menu.Target>
+                    <ActionIcon variant="default">
+                        <IconUserFilled/>
+                    </ActionIcon>
+                </Menu.Target>
+                <Menu.Dropdown>
+                    <Menu.Item onClick={toggleTheme}
+                               leftSection={
+                                   colorScheme === 'light' ?
+                                       <IconSun style={{width: rem(14), height: rem(14)}}/>
+                                       : <IconMoon style={{width: rem(14), height: rem(14)}}/>}>
+                        Przełącz motyw
+                    </Menu.Item>
+                    <Menu.Item onClick={logout}
+                               leftSection={<IconLogin style={{width: rem(14), height: rem(14)}}/>}>
+                        Wyloguj
+                    </Menu.Item>
+                </Menu.Dropdown>
+            </Menu>
+        </Group>
+    )
+}
--- src/views/layout/Layout.jsx
+++ src/views/layout/Layout.jsx
@@ -1,5 +1,32 @@
+import {useDisclosure} from "@mantine/hooks";
+import {AppShell, Burger, Group, Title} from "@mantine/core";
+import UserPanel from "../../components/layout/UserPanel.jsx";
+
 export default function Layout() {
+    const [opened, {toggle}] = useDisclosure();
     return (
-        <p>Zalogowano</p>
-    )
+        <AppShell
+            header={{height: 60}}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: {mobile: !opened},
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" style={{flexGrow: 0}}/>
+                    <Group justify="space-between" style={{flexGrow: 1}}>
+                        <Title order={2}>Cinemaster</Title>
+                        <UserPanel/>
+                    </Group>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
 }

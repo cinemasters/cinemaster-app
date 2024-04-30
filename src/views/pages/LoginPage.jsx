d369410580dcdd72134b7c54a7b87f001ce55082
TPGPL <TPGPL@users.noreply.github.com>
2024-05-01 00:59:33 +0200

    Tweak the login page

--- src/views/pages/LoginPage.jsx
+++ src/views/pages/LoginPage.jsx
@@ -1,8 +1,9 @@
-import {Alert, Button, Container, Loader, PasswordInput, Stack, TextInput} from "@mantine/core";
+import {Alert, Button, Container, Loader, Paper, PasswordInput, Stack, TextInput, Title} from "@mantine/core";
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {loginAction} from "../../actionCreators/auth.jsx";
+import {IconAlertTriangle} from "@tabler/icons-react";
 
 export default function LoginPage() {
     const [isLoading, setLoading] = useState(false);
@@ -50,22 +51,24 @@ export default function LoginPage() {
     }
 
     return (
-        <Container w={500} mt="20vh">
-            <h1>Cinemaster</h1>
-            <div>
-                {isError && (
-                    <Alert title="Nieprawidłowe dane logowania!" color="red"/>
-                )}
+        <Container w="30em" mt="25vh">
+            <Title order={1} mb="0.5em" ta="center">Cinemaster</Title>
+            <Paper withBorder p="lg" shadow="sm" radius="md">
                 <form onSubmit={login}>
                     <Stack gap="sm">
-                        <TextInput required name="username" label="Nazwa użytkownika" onChange={formInputChange}/>
-                        <PasswordInput required name="password" label="Hasło" onChange={formInputChange}/>
-                        <Button disabled={isLoading} type="submit">Zaloguj</Button>
-                        {isLoading && <Loader/>}
+                        {isError && (
+                            <Alert title="Nieprawidłowe dane logowania!" color="red" icon={<IconAlertTriangle/>}
+                                   autocontrast/>
+                        )}
+                        <TextInput required placeholder="Wprowadź nazwę użytkownika..." name="username"
+                                   label="Nazwa użytkownika" onChange={formInputChange}/>
+                        <PasswordInput required placeholder="Wprowadź hasło..." name="password" label="Hasło"
+                                       onChange={formInputChange}/>
+                        <Button disabled={isLoading} type="submit">{isLoading ? "Logowanie..." : "Zaloguj"}</Button>
+                        {isLoading && <Loader m="auto"/>}
                     </Stack>
                 </form>
-            </div>
+            </Paper>
         </Container>
-
     )
 }
